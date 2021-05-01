2ebd23d5fca39d5589218ab7c71f1d54db46bfb0
astrelowf <alexandre.alsf@gmail.com>
2021-04-30 23:57:19 -0300

    CodePen
    Conversor de moedas - Messi

---
+++ conversor-de-moedasmessi/README.markdown
@@ -0,0 +1,5 @@
+# Conversor de moedas - Messi
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/astrelowf/pen/yLgyMMX](https://codepen.io/astrelowf/pen/yLgyMMX).
+
+
---
+++ conversor-de-moedasmessi/dist/script.js
@@ -0,0 +1,10 @@
+var valorEmEurosNumero = parseFloat(prompt("Que valor, em euros, você deseja converter para reais?"))
+var valorEmReaisFixado = (valorEmEurosNumero * 6.517).toFixed(2)
+
+alert(valorEmReaisFixado)
+
+// Revisão
+// Variáveis (var) - string , int e float
+// Funções - alert, prompt, parseInt e parseFloat
+// Operações - soma (+) e multiplicação (*)
+// Comentários - //
